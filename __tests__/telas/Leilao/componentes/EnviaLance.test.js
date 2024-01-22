0ffaf19cdec2ec53351f89eca85b32b97e938a6b
GabrielBLucas11 <gabrielbl79@gmail.com>
2024-01-22 15:35:38 -0400

    Update EnviaLance.test.js and EnviaLance.js

--- __tests__/telas/Leilao/componentes/EnviaLance.test.js
+++ __tests__/telas/Leilao/componentes/EnviaLance.test.js
@@ -1,15 +1,33 @@
 import React from "react";
-import {render} from "@testing-library/react-native";
+import {jest} from "@jest/globals";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
 import EnviaLance from "../../../../src/telas/Leilao/componentes/EnviaLance";
+import { ENVIADO, NAO_ENVIADO } from "../../../../src/negocio/constantes/estadosLance";
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 describe('telas/Leilao/componentes/EnviaLance', () => {
-    it('Deve enviar  o lance quando o botão for pressionado', () => {
-        const { toJSON } = render(
+    it('Deve enviar o lance quando o botão for pressionado', async () => {
+        const enviaLance = jest.fn(() => new Promise(resolve => resolve(ENVIADO)));
+
+        const { getByPlaceholderText, getByA11yHint, getByText } = render(
             <EnviaLance 
-                enviaLance={() => {}} 
+                enviaLance={enviaLance} 
                 cor="#fff" 
             />);
 
-        console.log(toJSON());
+        const input = getByPlaceholderText('R$');
+        const botao = getByA11yHint('Enviar um lance');
+
+        fireEvent.changeText(input, '250');
+        fireEvent.press(botao);
+
+        expect(enviaLance).toHaveBeenCalledWith('250');
+        expect(enviaLance).toHaveBeenCalledTimes(1);
+        await waitFor(() => {
+            expect(getByText(ENVIADO)).toBeTruthy();
+        })
+
+        expect(() => getByText(NAO_ENVIADO)).toThrow();
     })
 });
--- src/telas/Leilao/componentes/EnviaLance.js
+++ src/telas/Leilao/componentes/EnviaLance.js
@@ -41,6 +41,7 @@ export default function EnviaLance({ enviaLance, cor }) {
       keyboardType="decimal-pad"
     />
     <TouchableOpacity 
+      accessibilityHint='Enviar um lance'
       onPress={validaEnvio}
       disabled={enviando}
       style={estilos.botao}>
